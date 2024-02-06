940e00c36c038ae6d5807160ad5cf36714afdfed
Neta Yaar <neta.yaar@post.runi.ac.il>
2024-02-06 20:02:19 +0200

    homework hopefullyyy final

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,86 +1,82 @@
-
 public class SpellChecker {
 
+
 	public static void main(String[] args) {
-		String word = "hello";
-		String word2 = "hallo";
-		System.out.println(levenshtein(word, word2));
-		//String word = args[0];
-		//int threshold = Integer.parseInt(args[1]);
-		//String[] dictionary = readDictionary("dictionary.txt");
-		//String correction = spellChecker(word, threshold, dictionary);
-		//System.out.println(correction);
+		String word = args[0];
+		int threshold = Integer.parseInt(args[1]);
+		String[] dictionary = readDictionary("dictionary.txt");
+		String correction = spellChecker(word, threshold, dictionary);
+		System.out.println(correction);
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
-		String tail = str.substring(1);
-		
-		return tail;
+		str = str.substring(1);
+
+		return str;
+	}
+
+	public static char head(String str) {
+		char strC = str.charAt(0);
+
+		return strC;
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
-		String w1 = word1.toLowerCase();
-		String w2 = word2.toLowerCase();
-		int lengthW1 = w1.length();
-		int lengthW2 = w2.length();
-		if (word1.isEmpty() && word2.isEmpty()) {
-			return 0;
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+
+		int w1Length = word1.length();
+		int w2Length = word2.length();
+
+		if (w1Length == 0) {
+			return w2Length;
 		}
-		// if one string is empty- returns the length of the word that is not empty
-		if (word1.isEmpty() && !word2.isEmpty()) {
-			return lengthW2;
+
+		if (w2Length == 0) {
+			return w1Length; 
 		}
-		if (!word1.isEmpty() && word2.isEmpty()) {
-			return lengthW1;
+
+		if (head(word1) == head(word2)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+
+		else {
+			int one = levenshtein(tail(word1), word2);
+			int two = levenshtein(word1, tail(word2));
+			int three = levenshtein (tail(word1), tail(word2));
+
+			int min = 1 + Math.min(one, Math.min(two, three));
+			return min; 
 
 		}
-		if (w1.charAt(0) == w2.charAt(0)) {
-			return levenshtein(tail(w1), tail(w2));
-		}
-		else {
-			//int cost = (word1.charAt(0) == word2.charAt(0)) ? 0 : 1;
-		// cost specifically relates to the substitution operation
-		
-		// one, two and three are the deletation, insertation and substitution
-			int deletation = levenshtein(tail(w1), w2);
-			int insertation = levenshtein(w1, tail(w2));
-			int substitution = levenshtein(tail(w1), tail(w2)) ;
-		// calculates the minimum between the operations
-			return Math.min(deletation, Math.min(insertation, substitution)) +1;
-	}}
+
+	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
 		for (int i = 0; i < dictionary.length; i++) {
-			String word = in.readString();
-			dictionary[i] = word;
+			dictionary[i] = in.readString();
 		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-				// Your code goes here
-	int bestValue = threshold;
-	String bestMatch = word.toLowerCase();
-	for (int i = 0; i < dictionary.length; i++) {
-		int currentValue = levenshtein(word, dictionary[i]);
-		if (currentValue < bestValue) {
-			bestValue = currentValue;
-			bestMatch = dictionary[i];}
+		word = word.toLowerCase();
+		String wordNow = word;
+		boolean found = false;
+		for (int i = 0; i < dictionary.length; i++) {
+			int threshNew = levenshtein(word, dictionary[i]);
 			
-		}if (bestValue >= threshold) {
-			return bestMatch;
-		} else{
-			 return word;
-		} 
+			if ((!found && threshNew <= threshold) || (found && threshNew < threshold)) {
+				threshold = threshNew;
+				found = true;
+				wordNow = dictionary[i];
+			}
 		}
-			
+		return wordNow;
 	}
-
+}
